437f67e0cdc1eb44ff92d945fd278013dd26d3c9
fek1s <xfukal01@stud.fit.vut.cz>
2024-10-06 14:45:31 +0200

    feat: add paser_arguments function

--- src/utility.c
+++ src/utility.c
@@ -1,6 +1,66 @@
 #include <stdio.h>
 #include "utility.h"
 
-void utility_func() {
-    printf("Hello from support function\n");
+
+void print_usage(){
+    fprintf(stderr, "Použití: dns-monitor (-i <interface> | -p <pcapfile>) [-v] [-d <domainsfile>] [-t <translationsfile>]\n");
+    //exit(EXIT_FAILURE);
+}               
+
+ProgramArguments parse_arguments(int argc, char * argv[]){
+    ProgramArguments args = {NULL, NULL, NULL, NULL, 0, 0};
+    int c;
+    while ((c = getopt(argc, argv, "i:p:d:t:vg")) != -1){
+        switch (c){
+            case 'i':
+                args.interface = optarg;
+                break;
+            case 'p':
+                args.pcapfile = optarg;
+                break;
+            case 'd':
+                args.domainsfile = optarg;
+                break;
+            case 't':
+                args.translationsfile = optarg;         
+                break;
+            case 'v':
+                args.verbose = 1;
+                break;
+            case 'g':
+                args.debug = 1;
+                break;
+            case '?':
+                print_usage();
+                break;
+            default:
+                print_usage();
+                break;
+        }
+    }
+
+    if (args.debug){
+        if ((args.interface && args.pcapfile) || (!args.interface && !args.pcapfile)){
+            print_usage();
+        }
+
+        if (args.interface){
+            printf("interface: %s\n", args.interface);
+        } else {
+            printf("pcapfile: %s\n", args.pcapfile);
+        }
+
+        if (args.domainsfile){
+            printf("domainsfile: %s\n", args.domainsfile);
+        }
+
+        if (args.translationsfile){
+            printf("translationsfile: %s\n", args.translationsfile);
+        }
+
+        if (args.verbose){
+            printf("Verbose option enabled\n");
+        } 
+    }
+    return args;
 }
--- src/utility.h
+++ src/utility.h
@@ -2,9 +2,41 @@
 #ifndef SUPP_H
 #define SUPP_H
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+
 /**
- * @brief Print a message
+ * @brief Holds the cli arguments passed to the program
+ *
+ * This structure holds all the necessary parameters that can be
+ * passed to the dns-monitor.
  */
-void utility_func();
+typedef struct
+{
+    char * interface;
+    char * pcapfile;
+    char * domainsfile;
+    char * translationsfile;
+    int verbose;
+    int debug;
+} ProgramArguments;
+
+/**
+ * @brief Prints the usage of the program to the standard error.
+ */
+void print_usage();
+
+/**
+ * @brief Parses the command-line arguments provided to the program.
+ *
+ * This function processes the arguments passed to the program and
+ * returns a structure containing the parsed values.
+ *
+ * @param argc The number of command-line arguments.
+ * @param argv An array of strings representing the command-line arguments.
+ * @return A ProgramArguments structure containing the parsed arguments.
+ */
+ProgramArguments parse_arguments(int argc, char * argv[]);
 
 #endif
