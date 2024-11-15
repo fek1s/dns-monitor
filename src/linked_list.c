d5c96bcf9985568550236e1f02d30167660dd77b
Jakub Fukala <xfukal01@stud.fit.vut.cz>
2024-11-15 14:51:21 +0100

    feat: add linked lists files

---
+++ src/linked_list.c
@@ -0,0 +1,7 @@
+/**
+ * @file linked_list.c
+ * @brief This file contains the implementation of the linked list used for 
+ * storing domain names and their translations.
+ * 
+ * @author Jakub Fukala (xfukal01)
+ */
