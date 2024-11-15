ce57aaeed2cf690499a0ccda51dc470573a9dfe6
Jakub Fukala <xfukal01@stud.fit.vut.cz>
2024-11-15 15:02:40 +0100

    fead: list skelet

--- src/linked_list.c
+++ src/linked_list.c
@@ -5,3 +5,29 @@
  * 
  * @author Jakub Fukala (xfukal01)
  */
+
+#include "linked_list.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+void init_domain_list(DomainList *list){
+    list->head = NULL;
+}
+
+int add_domain_name(DomainList *list, const char *domain_name){
+
+}
+
+int domain_exists(DomainList *list, const char *domain_name){
+
+}
+
+
+void free_domain_list(DomainList *list){
+
+}
+
+void write_domain_list(DomainList *list, const char *filename){
+    
+}
